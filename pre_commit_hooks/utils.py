98845bc920ebb8ca8f83b10d728b85dac4ccd9ce
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-09-13 15:16:32 -0700

    Update utils.py

--- pre_commit_hooks/utils.py
+++ pre_commit_hooks/utils.py
@@ -1,7 +1,20 @@
 """Utility functions"""
 
+import re
 
-def contains_tabs(filename: str):
+# Import this pattern on https://regex101.com/ for detailed explanation
+BEGINING_TABS_PATTERN: re.Pattern = re.compile(r"(^[ \t]+)([\S\s]*)")
+
+
+def contains_beginning_tabs(filename: str):
     """Check if `filename` contains tabs"""
     with open(filename, mode="rb") as file_checked:
-        return b"\t" in file_checked.read()
+        lines = file_checked.readlines()
+
+    for line in lines:
+        matcher = BEGINING_TABS_PATTERN.match(line)
+
+        if matcher is None:
+            return True
+
+    return False
